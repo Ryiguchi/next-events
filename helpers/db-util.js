ac14128e152fb4ee74fcb5be8ce02bfe0a698737
Ryan Iguchi <ryan.iguchi1@gmail.com>
2023-09-27 15:58:04 +0200

    dotenv

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -1,8 +1,8 @@
 import { MongoClient } from 'mongodb';
+import 'dotenv/config';
 
 export async function connectDatabase() {
-  const uri =
-    'mongodb+srv://ryaniguchi1:9NbuZtBHawCqsGeM@cluster0.ipifet7.mongodb.net/events?retryWrites=true&w=majority';
+  const uri = `mongodb+srv://ryaniguchi1:${process.env.MONGODB_PASSWORD}@cluster0.ipifet7.mongodb.net/events?retryWrites=true&w=majority`;
   return await MongoClient.connect(uri);
 }
 
